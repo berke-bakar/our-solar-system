ce1992c67154fb27d4ab8b1c5a3d22ebd26cf84a
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-07-25 18:28:00 +0300

    -> Setup design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        spartan: `"League Spartan", sans-serif`,
+        antonio: `"Antonio", sans-serif`,
+      },
     },
   },
   plugins: [],
