eb109ebbf10189f227304b9f24d13c80fc765d4a
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-07-25 18:28:59 +0300

    -> Implemented Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./reset.css";
+import Navbar from "@/components/Navbar";
+import data from "@/public/data.json";
 
 export const metadata: Metadata = {
   title: "☀️Our Lovely System🪐",
   description:
-    "Informative facts about planets in our lovely home platenary system: The Solar Sytem",
+    "Informative facts about planets (and Pluto) in our lovely home platenary system: The Solar Sytem",
 };
 
 export default function RootLayout({
@@ -13,9 +15,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const planetNames = data.map((val) => val.name.toLowerCase());
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar
+          title="The Solar System"
+          names={planetNames}
+          links={planetNames}
+        />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import React from "react";
+import { cn } from "@/utils/styleUtils";
+
+type NavbarProps = React.ComponentProps<"nav"> & {
+  names: string[];
+  links: string[];
+  title: string;
+};
+
+export default function Navbar({
+  names,
+  links,
+  title,
+  className,
+  ...props
+}: NavbarProps) {
+  if (names.length != links.length)
+    throw new Error("names and links props must have same length");
+
+  return (
+    <nav
+      className={cn(
+        "flex flex-row h-[85px] border-b-[1px] items-center px-8 border-gray-700 relative top-0 z-10",
+        className
+      )}
+      {...props}
+    >
+      <Link href={"/"} className="text-[1.75rem] font-antonio">
+        {title}
+      </Link>
+      <div className="flex flex-row grow gap-[33px] justify-end">
+        {names.map((val, ind) => {
+          return (
+            <li
+              key={ind}
+              className="list-none font-spartan text-s uppercase font-bold opacity-75"
+            >
+              <Link
+                href={links[ind]}
+                className="uppercase font-spart font-bold hover:opacity-100 opacity-75 text-s transition-opacity"
+              >
+                {val}
+              </Link>
+            </li>
+          );
+        })}
+      </div>
+    </nav>
+  );
+}
