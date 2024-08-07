06cd2302100af75f6cf0dc6b563d78e880de586e
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-08-08 00:53:01 +0300

    -> Added progress bar to suspense fallback loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -6,9 +6,21 @@ type LoaderProps = React.ComponentProps<typeof Html>;
 export default function Loader({ className, ...props }: LoaderProps) {
   const { progress } = useProgress();
 
+  const progressWidth = `w-[${Math.round(progress)}%]`;
+
   return (
     <Html center className={cn("animate-pulse", className)} {...props}>
-      Loading...{progress}%
+      <div className="flex flex-col justify-center items-stretch gap-4">
+        <h1>Loading...{progress}%</h1>
+        <div className="rounded-sm border-white border-2 h-[90px]">
+          <div
+            className={cn(
+              "bg-earth w-[0%] h-full transition-all",
+              progressWidth
+            )}
+          ></div>
+        </div>
+      </div>
     </Html>
   );
 }
