0a6f855ffbc82b5dfbadbd8447bdfba5734e3937
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-08-11 16:14:07 +0300

    -> Fixed suspense loader animation caused by tailwind

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -6,20 +6,16 @@ type LoaderProps = React.ComponentProps<typeof Html>;
 export default function Loader({ className, ...props }: LoaderProps) {
   const { progress } = useProgress();
 
-  const progressWidth = `w-[${Math.round(progress)}%]`;
+  const rounded = Math.round(progress);
 
   return (
     <Html center className={cn("animate-pulse", className)} {...props}>
       <div className="flex flex-col justify-center items-stretch gap-2 md:gap-6 lg:gap-8">
-        <h1 className={"text-lg md:text-[60px]"}>
-          Loading...{Math.round(progress)}%
-        </h1>
-        <div className="rounded-sm border-white border-2 h-[20px] md:h-[90px]">
+        <h1 className={"text-lg md:text-[60px]"}>Loading...{rounded}%</h1>
+        <div className="rounded-sm border-white border-2 h-[20px] md:h-[90px] w-full">
           <div
-            className={cn(
-              "bg-earth w-[0%] h-full transition-all",
-              progressWidth
-            )}
+            className={cn("bg-earth h-full transition-all")}
+            style={{ width: `${rounded}%` }}
           ></div>
         </div>
       </div>
